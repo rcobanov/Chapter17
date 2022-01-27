9ddd203992bc1eb1ba794df02b94afbdf9b405c6
robin <robin.cobanov@hotmail.com>
2022-01-27 20:45:45 +0100

    Task 13 completed.

--- src/se/hkr/Task13/UniqueWords.java
+++ src/se/hkr/Task13/UniqueWords.java
@@ -7,9 +7,10 @@ import java.util.regex.Pattern;
 public class UniqueWords {
     public static void main(String[] args) {
         // Input sentence from user.
-        Pattern pattern = Pattern.compile("\\s+");
         // Display unique words in alphabetic order.
-        //treat upper/lowercase the same
+        // treat upper/lowercase the same
+        Pattern pattern = Pattern.compile("\\s+");
+
         System.out.println("Input a sentence!");
         Scanner input = new Scanner(System.in);
         String myText = input.nextLine();
