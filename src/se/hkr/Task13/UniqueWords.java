f8dc96a36f86d0523654048810d67ed564dc17f6
robin <robin.cobanov@hotmail.com>
2022-01-27 20:29:52 +0100

    Task 12 Completed.

---
+++ src/se/hkr/Task13/UniqueWords.java
@@ -0,0 +1,25 @@
+package se.hkr.Task13;
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UniqueWords {
+    public static void main(String[] args) {
+        // Input sentence from user.
+        Pattern pattern = Pattern.compile("\\s+");
+        // Display unique words in alphabetic order.
+        //treat upper/lowercase the same
+        System.out.println("Input a sentence!");
+        Scanner input = new Scanner(System.in);
+        String myText = input.nextLine();
+
+        pattern.splitAsStream(myText)
+                .map(String::toLowerCase)
+                .distinct()
+                .sorted()
+                .forEach(System.out::println);
+
+
+    }
+}
